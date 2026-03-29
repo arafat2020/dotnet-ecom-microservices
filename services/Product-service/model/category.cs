7f4c0782e0661d579b9198ba324e79caaa637242
Arafat Mannan <arafatmannan@Arafats-Mac-mini.local>
2026-03-29 03:47:45 -0700

    MOD: Auth service initiated with Dto and database model

--- services/Product-service/model/category.cs
+++ services/Product-service/model/category.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace product_service.Model;
  
- [Index(nameof(Name), IsUnique = true)]
+[Index(nameof(Name), IsUnique = true)]
 public class CategoryModel
 {
     [Key]
